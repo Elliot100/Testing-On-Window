e9b61da4fdb0d6bff57fb00d89d21c9af8c9e2e9
Elly <ellychau10@yahoo.com>
2021-06-18 14:35:22 -0700

    Set up entry.jsx and ClickCounter Constructor

--- frontend/click_counter.jsx
+++ frontend/click_counter.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
 
+class ClickCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+}
+
 
 export default ClickCounter;
