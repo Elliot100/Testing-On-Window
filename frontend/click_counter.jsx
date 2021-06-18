267f0c3f593db81f0ba37274e2173ef392216dfa
Elly <ellychau10@yahoo.com>
2021-06-18 15:15:16 -0700

    Add render and click methods in ClickCounter

--- frontend/click_counter.jsx
+++ frontend/click_counter.jsx
@@ -3,7 +3,21 @@ import React from 'react';
 class ClickCounter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { counter: 0 };
+    this.state = { count: 0 };
+  }
+
+  click(event) {
+    event.preventDefault();
+    this.setState({ count: this.state.count + 1});
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.click.bind(this)}>CLICK ME!!!</button>
+        <span>{this.state.count}</span>
+      </div>
+    );
   }
 }
 
