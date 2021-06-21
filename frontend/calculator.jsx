abd9bdd4fb406dfada3d0d7b8925b7b93698e3e0
Elly <ellychau10@yahoo.com>
2021-06-21 11:41:52 -0700

    Initial Commit

---
+++ frontend/calculator.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return(
+      <dive>
+        <h1>Hello World</h1>
+      </dive>
+    );
+  }
+}
+
+
+export default Calculator;
