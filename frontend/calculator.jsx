c5695636b063a284c6fa435e4cf00d1593a41837
Elly <ellychau10@yahoo.com>
2021-06-21 11:56:51 -0700

    Update constructor

--- frontend/calculator.jsx
+++ frontend/calculator.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { result: 0, num1: '', num2: ''};
   }
 
   render(){
     return(
       <dive>
-        <h1>Hello World</h1>
+        <h1>{this.state.result}</h1>
       </dive>
     );
   }
